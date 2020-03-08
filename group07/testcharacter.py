43dab04bbe685df8e52b837a8dfd46eee84b59db
ndellicarpini <ndellicarpini22@gmail.com>
2020-03-07 19:01:48 -0500

    brue

--- group07/testcharacter.py
+++ group07/testcharacter.py
@@ -48,10 +48,10 @@ class TestCharacter(CharacterEntity):
 
     def build_file(self, wrld):
         try:
-            q_table = np.load('../q_table.npy', allow_pickle=True)
+            q_table = np.load('q_table.npy', allow_pickle=True)
         except IOError:
-            np.save('../q_table.npy', np.array([QEntry(self.wrldToState(wrld))], dtype=QEntry), allow_pickle=True)
-            q_table = np.load('../q_table.npy', allow_pickle=True)
+            np.save('q_table.npy', np.array([QEntry(self.wrldToState(wrld))], dtype=QEntry), allow_pickle=True)
+            q_table = np.load('q_table.npy', allow_pickle=True)
 
         return q_table
 
@@ -66,7 +66,7 @@ class TestCharacter(CharacterEntity):
         h = math.sqrt(dx * dx + dy * dy)
         return h
 
-    def neighbors(self, x, y, wrld):
+    def neighbors(self, x, y, wrld, walls):
 
         n = []
 
@@ -74,9 +74,12 @@ class TestCharacter(CharacterEntity):
             for j in range(y - 1, y + 2):
 
                 if (i, j) != (x, y) and i >= 0 and j >= 0 and i < wrld.width() and j < wrld.height():
-                    if not wrld.wall_at(i, j):
-                        if self.colorGrid:
-                            self.set_cell_color(x, y, Fore.CYAN)
+                    if walls:
+                        if not wrld.wall_at(i, j):
+                        #     if self.colorGrid:
+                        #         self.set_cell_color(x, y, Fore.CYAN)
+                            n.append((i, j))
+                    else:
                         n.append((i, j))
 
         return n
@@ -98,7 +101,8 @@ class TestCharacter(CharacterEntity):
         else:
             return local_path
 
-    def astar(self, start, goal, wrld):
+
+    def astar(self, start, goal, wrld, walls):
 
         frontier = PriorityQueue()
         frontier.put((0, start))
@@ -113,7 +117,7 @@ class TestCharacter(CharacterEntity):
             if current[1] == goal:
                 break
 
-            for next in self.neighbors(current[1][0], current[1][1], wrld):
+            for next in self.neighbors(current[1][0], current[1][1], wrld, walls):
                 new_cost = cost_so_far[current[1]] + self.cost(current, next, wrld)
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
@@ -121,8 +125,8 @@ class TestCharacter(CharacterEntity):
                     frontier.put((priority, next))
                     came_from[next] = current[1]
 
-        path = self.follow_path(start, came_from, wrld.exitcell, [])
-        # print(path)
+        temp = self.follow_path(start, came_from, wrld.exitcell, [])
+        path = temp if temp or not walls else self.astar(start, goal, wrld, False)
         for i in path:
             if i is not None and self.colorGrid:
                 self.set_cell_color(i[0], i[1], Fore.RED)
@@ -141,7 +145,7 @@ class TestCharacter(CharacterEntity):
     def wrldToState(self, wrld):
         state_dic = {}
 
-        a_star = self.astar((self.x, self.y), wrld.exitcell, wrld)
+        # a_star = self.astar((self.x, self.y), wrld.exitcell, wrld)
 
         for x in range(-2, 3):
             for y in range(-2, 3):
@@ -171,21 +175,22 @@ class TestCharacter(CharacterEntity):
 
                 else:
                     state_dic[(x, y)] = 'o'
-        if len(a_star):
-            state_dic['a_star'] = (a_star[0][0] - self.x, a_star[0][1] - self.y)
+        # if len(a_star):
+        #     state_dic['a_star'] = (a_star[0][0] - self.x, a_star[0][1] - self.y)
+        # else:
+        #     state_dic['dist'] = self.y - wrld.exitcell[1]
 
         return state_dic
 
-    def qLearn(self, wrld):
+    def qLearn(self, wrld, state):
 
         # constants
         alpha = 0.8
         gamma = 0.8
         epsilon = 0
-        state = self.wrldToState(wrld)
         action = (0, 0, -1)
 
-        if random.uniform(0, 1) > epsilon:
+        if random.uniform(0, 1) >= epsilon:
             for i in self.q_table:
                 if i.state == state:
                     if max(i.action_value.values()) == 0:
@@ -209,46 +214,58 @@ class TestCharacter(CharacterEntity):
 
         sensed_world = SensedWorld.from_world(wrld)
         next_state, next_events = sensed_world.next()
-        reward = 5000 / next_state.scores[self.name]
+        reward = 5000 / next_state.scores[self.name] * 0.3
         if len(next_events) > 0:
             for i in next_events:
                 if i.tpe == 0 and i.character == self:
                     print('nice bomb')
-                    reward += (1 / self.heuristic(((self.x + action[0]), (self.y + action[1])), next_state.exitcell)) * 10
+                    reward += 10
                 if i.tpe == 1 and i.character == self:
                     print('niiice bomb')
                     reward += 50
-                # if i.tpe == 2:
-                #     if i.character == self and i.character != i.other:
-                #         print('shmoney bomb')
-                #         reward += 100
-                #     elif i.character == self and i.character == i.other:
-                #         print('bad bomd')
-                #         reward -= 1000
+                if i.tpe == 2:
+                    if i.character == self and i.character != i.other:
+                        print('shmoney bomb')
+                        reward += 100
+                    elif i.character == self and i.character == i.other:
+                        print('bad bomd')
+                        reward -= 500
 
         if (self.x + action[0]) >= next_state.width() or (self.y + action[1]) >= next_state.height() or (
                 self.x + action[0]) < 0 or (self.y + action[1]) < 0:
             reward -= 10
+        elif next_state.explosion_at((self.x + action[0]), (self.y + action[1])):
+            reward -= 500
+        elif next_state.monsters_at((self.x + action[0]), (self.y + action[1])):
+            reward -= 500
         elif next_state.wall_at((self.x + action[0]), (self.y + action[1])):
             reward -= 10
-        elif next_state.explosion_at((self.x + action[0]), (self.y + action[1])):
-            reward -= 1000
-        elif next_state.monsters_at(self.x, self.y):
-            reward -= 1000
+
+        # for i in self.neighbors((self.x + action[0]), (self.y + action[1]), next_state):
+        #     if next_state.monsters_at(i[0], i[1]):
+        #         reward -= 100
 
         if self.heuristic(
                 ((self.x + action[0]), (self.y + action[1])), next_state.exitcell) == 0:
             reward += 1000
 
-        else:
+        # else:
             # print('A- Star Rewards')
             # print(action[0], state['a_star'][0])
             # print(action[1], state['a_star'][1])
-            if 'a_star' in state:
-                reward += (abs(action[0] + state['a_star'][0])) + (abs(action[1] + state['a_star'][1])) - 2
-            else:
-                temp = self.heuristic(((self.x + action[0]), (self.y + action[1])), next_state.exitcell)
-                reward += (1 / temp) * 2 if temp < self.heuristic((self.x, self.y), next_state.exitcell) else -1
+            # if 'a_star' in state:
+            #     if wrld.wall_at(state['a_star'][0], state['a_star'][1]):
+            #         reward += action[2] * 20
+            #
+            #     if state['a_star'][0] == action[0]:
+            #         reward += 2
+            #
+            #     if state['a_star'][1] == action[1]:
+            #         reward += 2
+            #
+            # else:
+            #     temp = wrld.exitcell[1] - self.y
+            #     reward += (100 * 1/temp if temp > 0 else 1000) if temp < (temp + action[1]) else 0
 
         next_state = self.wrldToState(next_state)
 
@@ -280,8 +297,9 @@ class TestCharacter(CharacterEntity):
             new_entry.action_value[action] = new_value
             self.q_table = np.append(self.q_table, [new_entry])
 
+        print(reward)
         if self.show_rewards:
-            file = open('../rewards.csv', 'a')
+            file = open('rewards.csv', 'a')
             file.write('(' + str(self.x) + ' ' + str(self.y) + '),(' + str(action[0]) + ' ' + str(action[1]) + ' ' + str(action[2]) + '),' + str(reward))
 
         return action
@@ -307,12 +325,27 @@ class TestCharacter(CharacterEntity):
         #
         # self.move(dx, dy)
         self.q_table = self.build_file(wrld)
-        dx, dy, bomb = self.qLearn(wrld)
-        self.move(dx, dy)
-        if bomb:
-            self.place_bomb()
+        state = self.wrldToState(wrld)
+        if ('m' in state.values()) or ('x' in state.values()):
+            dx, dy, bomb = self.qLearn(wrld, state)
+            self.move(dx, dy)
+            if bomb:
+                self.place_bomb()
+            np.save('q_table.npy', self.q_table, allow_pickle=True)
+        else:
+            a_star = self.astar((self.x, self.y), wrld.exitcell, wrld, True)
+            if a_star:
+                dx, dy = a_star[0]
+                dx = dx - self.x
+                dy = dy - self.y
+                print(self.x, self.y)
+                self.move(dx, dy)
+                if wrld.wall_at(self.x + dx, self.y + dy):
+                    self.place_bomb()
+            else:
+                dx = wrld.exitcell[0] - self.x
+                dy = wrld.exitcell[1] - self.y
+                self.move(dx, dy)
 
-        print(len(self.q_table))
-        np.save('../q_table.npy', self.q_table, allow_pickle=True)
         # for i in self.q_table:
         #     print(i.action_value)
